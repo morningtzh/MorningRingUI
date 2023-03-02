9fc9762696e8e938ebd512fa78f73caa76f0c933
MorningTZH <morningtzh@yeah.net>
2023-03-02 23:51:50 +0800

    CDN设置完成
    Signed-off-by: MorningTZH <morningtzh@yeah.net>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,23 @@
 
+export PUBLIC_VERSION="1.0.`date +%s`"
+export BUILD_PATH="build/publish"
+
+echo Version: ${PUBLIC_VERSION}
+
 # 创建输出路径
 rm -rf build
-
 git clone https://github.com/morningtzh/MorningRingUIPublish.git build
 
+# file like:
+# https://cdn.jsdelivr.net/gh/[username]/[repo]@[branch/tag/commitid]/[path]/[to]/[file]
+# https://cdn.jsdelivr.net/gh/morningtzh/MorningRingUIPublish@1.0.1677771068/publish/static/js/main.6128cea3.js
+# Docs: https://www.jsdelivr.com/documentation#id-github
+export PUBLIC_URL="https://cdn.jsdelivr.net/gh/morningtzh/MorningRingUIPublish@${PUBLIC_VERSION}/publish/"
 
 npm run build
+
+cd build
+git checkout -b ${PUBLIC_VERSION}
+git add *
+git commit -m "Version: ${PUBLIC_VERSION}"
+git push --set-upstream origin ${PUBLIC_VERSION}
